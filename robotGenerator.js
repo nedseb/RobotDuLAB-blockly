ab5df81c2a676d5fbdf94f05dc5ad335b009d688
Sébastien NEDJAR <nedseb@goldaire.com>
2016-12-13 14:53:12 +0100

    Update robotGenerator.js

--- robotGenerator.js
+++ robotGenerator.js
@@ -14,6 +14,12 @@ var define_robotdulab = '#include <Servo.h>\n'+
   '#define ANNEAU_LED_PIN 6\n\n'+
   'RobotDuLAB robotDuLAB(TRIGGER_PIN, ECHO_PIN, ROUE_DROITE_PIN, ROUE_GAUCHE_PIN, ANNEAU_LED_PIN);\n';
 
+Blockly.Arduino ['Avancer_Et_Attendre'] = function (block)  {
+  var time = Blockly.Arduino.valueToCode(block, 'DELAY_TIME', Blockly.Arduino.ORDER_ATOMIC);
+  var code  ="robotDuLAB.Avancer();\ndelay("+time+");\n";
+  return  code;
+};
+
 Blockly.Arduino ['Avancer'] = function (block)  {
   Blockly.Arduino.setups_["setup_robotdulab"] = setup_robotdulab;
   Blockly.Arduino.definitions_["define_robotdulab"] = define_robotdulab;
